611f26516bbcf9317e9b1593f9f6d971a2b2c19f
Hugo Lima Romão <hugo8romao@gmail.com>
2022-02-21 07:45:26 -0400

    feat: rat in a maze instance

--- 2021.2/POO/prova/RatInMaze.java
+++ 2021.2/POO/prova/RatInMaze.java
@@ -8,7 +8,7 @@ public class RatInMaze extends Thread {
 
   int n = N, m = M;
 
-  private static boolean[][] visited = new boolean[N][M];
+  private boolean[][] visited = new boolean[N][M];
 
   public RatInMaze(String name, int maze[][]) {
     this.name = name;
@@ -16,24 +16,45 @@ public class RatInMaze extends Thread {
   }
 
   public static void main(String[] args) {
-    setVisited(true);
+    // setVisited(true);
 
-    if (isReachable(maze)) {
-      System.out.println("Path Found!\n");
-    } else
-      System.out.println("No Path Found!\n");
+    // if (isReachable(maze)) {
+    // System.out.println("Path Found!\n");
+    // } else
+    // System.out.println("No Path Found!\n");
+  }
+
+  public void transformMaze() {
+    for (int i = 0; i < RatInMaze.maze.length; i++) {
+      for (int j = 0; j < RatInMaze.maze[i].length; j++) {
+        if (RatInMaze.maze[i][j] == 42) { // *
+          RatInMaze.maze[i][j] = 0;
+        }
+        if (RatInMaze.maze[i][j] == 69) { // E
+          RatInMaze.maze[i][j] = 1;
+        }
+        if (RatInMaze.maze[i][j] == 64) { // @
+          RatInMaze.maze[i][j] = 1;
+        }
+        if (RatInMaze.maze[i][j] == 83) { // S
+          RatInMaze.maze[i][j] = 1;
+        }
+      }
+    }
   }
 
   @Override
   public void run() {
-    setVisited(true);
-    if(isReachable(maze)) {
+    transformMaze();
+    setVisited(true, visited);
+    if (isReachable(maze, name, visited)) {
       System.out.printf("%s chegou primeiro!\n", name);
       System.exit(0);
-    };
+    }
+    ;
   }
 
-  private static void setVisited(boolean b) {
+  private static void setVisited(boolean b, boolean[][] visited) {
     for (int i = 0; i < visited.length; i++) {
       for (int j = 0; j < visited[i].length; j++) {
         visited[i][j] = b;
@@ -42,7 +63,7 @@ public class RatInMaze extends Thread {
 
   }
 
-  private static boolean isReachable(int maze[][]) {
+  private static boolean isReachable(int maze[][], String ratName, boolean[][] visited) {
     // Start position
     int i = 1, j = 1;
 
@@ -86,7 +107,8 @@ public class RatInMaze extends Thread {
           Node temp1 = new Node(i - 1, j);
           visited[i - 1][j] = false;
           s.push(temp1);
-          System.out.printf("go up\n");
+          // System.out.println(d);
+          System.out.printf("%s: foi para cima\n", ratName);
         }
       } else if (d == 1) {
         // Checking the left direction
@@ -95,7 +117,8 @@ public class RatInMaze extends Thread {
           Node temp1 = new Node(i, j - 1);
           visited[i][j - 1] = false;
           s.push(temp1);
-          System.out.printf("go left\n");
+          // System.out.println(d);
+          System.out.printf("%s: foi para esquerda\n", ratName);
         }
       } else if (d == 2) {
         // Checking the down direction
@@ -104,7 +127,8 @@ public class RatInMaze extends Thread {
           Node temp1 = new Node(i + 1, j);
           visited[i + 1][j] = false;
           s.push(temp1);
-          System.out.printf("go down\n");
+          // System.out.println(d);
+          System.out.printf("%s: foi para baixo\n", ratName);
         }
       } else if (d == 3) {
         // Checking the right direction
@@ -113,7 +137,8 @@ public class RatInMaze extends Thread {
           Node temp1 = new Node(i, j + 1);
           visited[i][j + 1] = false;
           s.push(temp1);
-          System.out.printf("go right\n");
+          // System.out.println(d);
+          System.out.printf("%s: foi para direita\n", ratName);
         }
       }
 
@@ -121,7 +146,8 @@ public class RatInMaze extends Thread {
       else {
         visited[temp.getX()][temp.getY()] = true;
         s.pop();
-        System.out.printf("go back\n");
+        // System.out.println(d);
+        System.out.printf("%s: voltou\n", ratName);
       }
     }
 
